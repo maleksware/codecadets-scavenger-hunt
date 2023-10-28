b358c70106f2dd6d0b45eaa8375006236f3a113c
Alex Maksimov <maleksware@gmail.com>
2023-10-28 17:48:34 +1100

    first commit

---
+++ checker.py
@@ -0,0 +1,76 @@
+from flask import Flask, request, make_response, render_template
+import uuid
+
+# База архитектуры: регистрация пользователя при сканировании первого кода, генерация рандомных ID на QR-кодах
+# На устройстве храним cookie с айдишником
+
+global registered_users
+registered_users = {}
+station_mapping = {}
+
+next_station = {"start": 4779, 4779: 9262, 9262: 3146, 3146: 1668, 1668: 9145, 9145: 6596, 6596: 3374}
+
+station_uuids = [4779, 9262, 3146, 1668, 9145, 6596, 3374]
+station_questions = [
+    "Welcome to the ScaNvenger Hunt! Go to the printer on level 3 for your next clue.",
+    "-... .. -. / --- -. / .-.. ...- .-.. / ..--- ",
+    "01000011 01101000 01100101 01100011 01101011 00100000 01110100 01101000 01100101 00100000 01010000 01101001 01100001 01101110 01101111",
+    "⠝⠑⠭⠞ ⠉⠇⠥⠑ ⠤ ⠇⠧⠇ ⠒ ⠎⠉⠥⠇⠏⠞⠥⠗⠑",
+    "R28gdG8gdGhlIG1haW50ZW5hbmNlIGN1cGJvYXJkIG9uIEx2bCAy=",
+    "43 68 65 63 6b 20 74 68 65 20 46 6f 6f 64 20 43 6f 72 6e 65 72 20 6f 6e 20 4c 76 6c 20 31",
+    "Congratulations! You have completed the ScaNvenger Hunt!"
+    ]
+
+
+class Station:
+    def __init__(self, question, id, number):
+        self.question = question
+        self.id = id
+        self.number = number
+
+
+class User:
+    def __init__(self, uuid, progress):
+        self.uuid = uuid
+        self.progress = progress
+
+
+for i in range(len(station_questions)):
+    station_mapping[station_uuids[i]] = Station(station_questions[i], station_uuids[i], i + 1)
+
+app = Flask(__name__)
+
+@app.route("/start")
+def reg_new_user():
+    global registered_users
+    new_user_id = str(uuid.uuid4())
+
+    resp = make_response(render_template("index.html", question=station_mapping[next_station["start"]].question))
+    resp.set_cookie("uuid", new_user_id)
+
+    registered_users[new_user_id] = User(new_user_id, 0)
+
+    return resp
+
+@app.route("/station/<int:station_id>")
+def process_station(station_id):
+    print(station_id)
+    if station_id in station_mapping:
+        cur_user_id = request.cookies.get("uuid")
+        global registered_users
+        print(cur_user_id, registered_users)
+
+        if cur_user_id in registered_users:
+            cur_station = station_mapping[station_id].number
+            user_progress = registered_users[cur_user_id].progress # last visited station
+            print(cur_station, user_progress)
+
+            if cur_station <= user_progress + 1:
+                registered_users[cur_user_id].progress = cur_station
+                return make_response(render_template("index.html", question=station_mapping[next_station[station_id]].question))
+            else:
+                return make_response(render_template("dont_shortcut.html"))
+        else:
+            return make_response(render_template("not_registered.html"))
+    else:
+        return make_response(render_template("no_station.html"))
